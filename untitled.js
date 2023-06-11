d204610103ab6056d41e6bd26ac7e69ab16e06fe
ruby9915 <129141602+ruby9915@users.noreply.github.com>
2023-06-11 17:53:27 +0900

    rucd 분리

--- untitled.js
+++ untitled.js
@@ -1,135 +1,75 @@
-const readline = require('readline');
-const fs = require('fs');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+    const memoInput = document.getElementById('memoInput');
+    const memoList = document.getElementById('memoList');
 
-const notesFile = 'notes.txt';
+    function insertNote() {
+    const memo = memoInput.value;
+    if (memo.trim() !== '') {
+        memoList.innerHTML += `<li>${memo}</li>`;
+        alert(`메모가 저장되었습니다:${memo}`);
+        memoInput.value = '';
+    }
+    }
 
-function insertNote() {
-    rl.question('메모를 입력하세요: ', (note) => {
-        fs.appendFile(notesFile, note + '\n', (err) => {
-            if (err) throw err;
-            console.log('메모가 저장되었습니다.');
-            showMenu();
-        });
-    });
-}
-
-function searchNote() {
-    rl.question('검색할 키워드를 입력하세요: ', (keyword) => {
-        fs.readFile(notesFile, 'utf8', (err, data) => {
-            if (err) throw err;
-            const notes = data.split('\n');
-            const matchingNotes = notes.filter((note) => note.includes(keyword));
-            if (matchingNotes.length > 0) {
-                console.log('검색 결과:');
-                matchingNotes.forEach((note) => {
-                    console.log(note);
-                });
-            } else {
-                console.log('검색 결과가 없습니다.');
-            }
-            showMenu();
-        });
-    });
-}
-
-function editNote() {
-    rl.question('수정할 메모의 번호를 입력하세요: ', (noteNumber) => {
-        fs.readFile(notesFile, 'utf8', (err, data) => {
-            if (err) throw err;
-            const notes = data.split('\n');
-            if (noteNumber >= 1 && noteNumber <= notes.length) {
-                rl.question('새로운 메모를 입력하세요: ', (newNote) => {
-                    notes[noteNumber - 1] = newNote;
-                    fs.writeFile(notesFile, notes.join('\n'), (err) => {
-                        if (err) throw err;
-                        console.log('메모가 수정되었습니다.');
-                        showMenu();
-                    });
-                });
-            } else {
-                console.log('잘못된 메모 번호입니다.');
-                showMenu();
-            }
-        });
-    });
-}
-
-function deleteNote() {
-    rl.question('삭제할 메모의 번호를 입력하세요: ', (noteNumber) => {
-        fs.readFile(notesFile, 'utf8', (err, data) => {
-            if (err) throw err;
-            const notes = data.split('\n');
-            if (noteNumber >= 1 && noteNumber <= notes.length) {
-                notes.splice(noteNumber - 1, 1);
-                fs.writeFile(notesFile, notes.join('\n'), (err) => {
-                    if (err) throw err;
-                    console.log('메모가 삭제되었습니다.');
-                    showMenu();
-                });
-            } else {
-                console.log('잘못된 메모 번호입니다.');
-                showMenu();
-            }
-        });
-    });
-}
-
-function displayNotes() {
-    fs.readFile(notesFile, 'utf8', (err, data) => {
-        if (err) throw err;
-        const notes = data.split('\n');
-        console.log('===== 전체 메모 내용 =====');
-        notes.forEach((note, index) => {
-            if (note.trim() !== '') {
-                console.log(`${index + 1}. ${note}`);
-            } //공백에도 번호가 붙어서 출력되는걸 방지
-        });
-        console.log('=======================');
-        showMenu();
-    });
-}
-//전체 메모 내역 확인
-
-function showMenu() {
-    console.log('===== 메모장 =====');
-    console.log('1. 메모 삽입');
-    console.log('2. 메모 검색');
-    console.log('3. 메모 수정');
-    console.log('4. 메모 삭제');
-    console.log('5. 메모 확인');
-    console.log('0. 종료');
-    console.log('=================');
-    rl.question('원하는 작업을 선택하세요: ', (choice) => {
-        switch(choice) {
-            case '1':
-                insertNote();
-                break;
-            case '2':
-                searchNote();
-                break;
-            case '3':
-                editNote();
-                break;
-            case '4':
-                deleteNote();
-                break;
-            case '5':
-                displayNotes();
-                break;
-            case '0':
-                rl.close();
-                break;
-            default:
-                console.log('잘못된 선택입니다.');
-                showMenu();
-                break;
+    function searchNote() {
+    const keyword = memoInput.value;
+    if (keyword.trim() !== '') {
+        const notes = memoList.getElementsByTagName('li');
+        let foundNotes = [];
+        for (let i = 0; i < notes.length; i++) {
+        const note = notes[i].textContent;
+        if (note.includes(keyword)) {
+            foundNotes.push(note);a
         }
-    });
-}
+        }
+        if (foundNotes.length > 0) {
+        alert(`검색 결과:\n${foundNotes.join('\n')}`);
+        } else {
+        alert('검색 결과가 없습니다.');
+        }
+        memoInput.value = '';
+    }
+    }
 
-showMenu();
+    function editNote() {
+    const noteNumber = memoInput.value;
+    const notes = memoList.getElementsByTagName('li');
+    if (noteNumber >= 1 && noteNumber <= notes.length) {
+        const newNote = prompt('새로운 메모를 입력하세요:');
+        if (newNote !== null) {
+        notes[noteNumber - 1].textContent = newNote;
+        alert('메모가 수정되었습니다.');
+        }
+        memoInput.value = '';
+    } else {
+        alert('잘못된 메모 번호입니다.');
+    }
+    }
+
+    function deleteNote() {
+    const noteNumber = memoInput.value;
+    const notes = memoList.getElementsByTagName('li');
+    if (noteNumber >= 1 && noteNumber <= notes.length) {
+        if (confirm('정말로 메모를 삭제하시겠습니까?')) {
+        notes[noteNumber - 1].remove();
+        alert('메모가 삭제되었습니다.');
+        }
+        memoInput.value = '';
+    } else {
+        alert('잘못된 메모 번호입니다.');
+    }
+    }
+
+    function displayNotes() {
+    const notes = memoList.getElementsByTagName('li');
+    if (notes.length > 0) {
+        let allNotes = [];
+        for (let i = 0; i < notes.length; i++) {
+        allNotes.push(notes[i].textContent);
+        }
+        alert(`전체 메모 내용:\n${allNotes.join('\n')}`);
+    } else {
+        alert('메모가 없습니다.');
+    }
+    memoInput.value = '';
+    }
